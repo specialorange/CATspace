cf04b491e7252ac4e9f68417ab27463dce782eef
s-edwards <edwards@cs.vt.edu>
2013-01-21 16:08:04 -0500

    porting history from old code

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,5 @@
+require 'mongrel_cluster/recipes'
+
 set :application, "catspace.web-cat.org"
 set :user, "deploy"
 set :repository,  "http://subversion.assembla.com/svn/catspace"
@@ -6,6 +8,9 @@ set :deploy_to, "/home/deploy/catspace"
 role :app, "catspace.web-cat.org"
 role :web, "catspace.web-cat.org"
 role :db,  "catspace.web-cat.org", :primary => true
+
+# Cluster Config
+set :mongrel_conf, "#{current_path}/config/mongrel_cluster.yml"
 
 #We're going to deploy via workstation; i.e., the server never accesses the SVN. The workstation does that, then zips up the file and send it via ssh to the remote server.
 set :deploy_via, :copy
